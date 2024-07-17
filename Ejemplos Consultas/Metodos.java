5a8b055aeac2a695abc10af8732f146f66dcafdc
lCristianl <cjortega03@tecnologicosudamericano.edu.ec>
2024-07-17 15:13:32 -0500

    Segundo commit - Metodos

--- Ejemplos Consultas/Metodos.java
+++ Ejemplos Consultas/Metodos.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Metodos{
-    public void Login(String nombre, int contraseña){
-        if (nombre.equalsIgnoreCase("Cristian") && contraseña == 2005){
+    public void Login(String nombre, String contraseña){
+        if (nombre.equalsIgnoreCase("Cristian") && contraseña.equals("2005")){
             System.out.println("----------¡Login Exitoso!----------");
             System.out.println("Hola " + nombre);
         } else {
-            System.out.println("¡Error en el usuario o la contraseña!");
+            System.out.println("¡Error en el usuario o la clave!");
         }
     }
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class Metodos{
         System.out.println("---------------Login---------------");
         System.out.println("Escriba su usuario: ");
         String nombre = entrada.nextLine();
-        System.out.println("Escriba su contraseña: ");
+        System.out.println("Escriba su clave: ");
         int contraseña = entrada.nextInt();
 
         Metodos login = new Metodos();
